9398ad980159cdf60f779823bbf5aff9b4988dfa
VukV <vuk.vukovic.2000@gmail.com>
2022-08-09 17:30:15 +0200

    init

---
+++ src/views/ConcertsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TitleComponent title="Concerts" description="Find all concerts in one place!"></TitleComponent>
+  </div>
+</template>
+
+<script>
+import TitleComponent from "@/components/TitleComponent";
+
+export default {
+  name: 'ConcertsView',
+  components: {
+    TitleComponent
+  }
+}
+</script>
---
+++ src/views/ReservationsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TitleComponent title="My Reservations" description="View and manage your reservations"></TitleComponent>
+  </div>
+</template>
+
+<script>
+import TitleComponent from "@/components/TitleComponent";
+
+export default {
+  name: 'ReservationsView',
+  components: {
+    TitleComponent
+  }
+}
+</script>
