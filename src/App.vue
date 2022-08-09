57cc810aafcb7474c274260444b200909a2fec17
VukV <vuk.vukovic.2000@gmail.com>
2022-08-09 17:25:20 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <NavbarComponent></NavbarComponent>
+
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from "@/components/NavbarComponent";
+export default {
+  components: {
+    NavbarComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent'
+}
+</script>
+
+<style scoped>
+
+</style>
